b382fd22c94501951e4aed253d0fdbec72cc898f
adamcklaam <adam4680@gmail.com>
2026-09-23 16:42:27 -0400

    CCT360 lab2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("waterButton").onclick = function(){
+    document.getElementById("plant").innerHTML = "🌿";
+
+    document.getElementById("message").innerHTML = "Your plant looks happier!";
+
+    document.getElementById("growth").innerHTML = "Growth: 25%";
+};
